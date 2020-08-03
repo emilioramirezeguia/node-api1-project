10e09d524c6e4ee039569f623fbae21a5a0c59d2
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-03 13:38:09 -0700

    Add error handling for creating a new user

--- server.js
+++ server.js
@@ -6,22 +6,50 @@ server.use(express.json());
 
 let users = [];
 
+// Make sure server is working with a request to the home route
 server.get("/", (req, res) => {
   res.send("Server is working!");
 });
 
+// Create a user
 server.post("/api/users", (req, res) => {
   const user = req.body;
+
+  if (!user.name) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide a name for the user" });
+  } else if (!user.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Don't forget to add the user's bio" });
+  }
 
   user.id = shortid();
 
   users.push(user);
 
-  res.status(201).json(users);
+  res
+    .status(201)
+    .json({ successMessage: "Successfully created user", user: user });
 });
 
+// Get a list of users
 server.get("/api/users", (req, res) => {
   res.status(200).json(users);
+});
+
+// Get a user with a specified id
+server.get("/api/users/:id", (req, res) => {
+  const userID = req.params.id.toLocaleLowerCase();
+
+  users.map((user) => {
+    if (user.id.toLocaleLowerCase() === userID) {
+      res.status(200);
+    } else {
+      res.status(404).send(`User with ID ${userID} not found.`);
+    }
+  });
 });
 
 const port = 8000;
