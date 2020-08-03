81227b8cb20647bc9c752f6101f6f38a50e4dfac
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-03 13:12:15 -0700

    Create a user and get list of users

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const server = express();
+server.use(express.json());
+
+let users = [];
+
+server.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+
+  user.id = shortid();
+
+  users.push(user);
+
+  res.status(201).json(users);
+});
+
+server.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
 
 const port = 8000;
 server.listen(port, () => {
