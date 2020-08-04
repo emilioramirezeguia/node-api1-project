528b82394ff860015bd4315945873798e8c1e7e0
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-03 22:31:48 -0700

    Delete and update user by id

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ server.get("/", (req, res) => {
   res.send("Server is working!");
 });
 
-// Create a user
+// POST new user
 server.post("/api/users", (req, res) => {
   const user = req.body;
 
   if (!user.name) {
-    res
+    return res
       .status(400)
       .json({ errorMessage: "Please provide a name for the user." });
   } else if (!user.bio) {
-    res
+    return res
       .status(400)
       .json({ errorMessage: "Don't forget to add the user's bio." });
   }
@@ -29,50 +29,75 @@ server.post("/api/users", (req, res) => {
 
   users.push(user);
 
-  res
-    .status(201)
-    .json({ successMessage: "Successfully created user.", user: user });
+  res.status(201).json({ successMessage: "Successfully created user.", user });
 });
 
-// Get a list of users
+// GET list of users
 server.get("/api/users", (req, res) => {
-  try {
-    if (users.length) {
-      res
-        .status(200)
-        .json({ successMessage: "Here's your list of users.", users: users });
-    } else {
-      res.status(200).json({ successMessage: "No users created yet." });
-    }
-  } catch (error) {
-    res.status(500).json({
-      errorMessage: "There was an error retrieving the list of users.",
-    });
+  if (users.length) {
+    res
+      .status(200)
+      .json({ successMessage: "Here's your list of users.", users });
+  } else {
+    res.status(200).json({ successMessage: "No users created yet." });
   }
 });
 
-// Get a user with a specified id
+// GET user by id
 server.get("/api/users/:id", (req, res) => {
-  try {
-    const userID = req.params.id;
-    console.log("Searching for user ID: ", userID);
+  const userID = req.params.id;
+  const user = users.find((user) => user.id === userID);
 
-    users.map((user) => {
-      if (user.id === userID) {
-        res.status(200).json({
-          successMessage: `Here's the user with ID '${userID}'.`,
-          user: user,
-        });
-      } else {
-        res
-          .status(404)
-          .json({ errorMessage: `User with ID '${userID}' was not found.` });
-      }
+  if (user) {
+    res.status(200).json({
+      successMessage: `Here's the user with ID '${userID}'`,
+      user,
     });
-  } catch (error) {
-    res.status(500).json({
-      errorMessage: "There was an error retrieving the user's information.",
+  } else {
+    res
+      .status(404)
+      .json({ errorMessage: `The user with ID '${userID}' was not found.` });
+  }
+});
+
+// DELETE user by id and return deleted user
+server.delete("/api/users/:id", (req, res) => {
+  const userID = req.params.id;
+  const deletedUser = users.find((user) => user.id === userID);
+
+  if (deletedUser) {
+    users = users.filter((user) => user.id !== userID);
+    res.status(200).json({
+      successMessage: `User with ID '${userID}' was successfully deleted`,
+      deletedUser,
     });
+  } else {
+    res
+      .status(404)
+      .json({ errorMessage: `The user with ID '${userID}' was not found.` });
+  }
+});
+
+// UPDATE user by id
+server.put("/api/users/:id", (req, res) => {
+  const userID = req.params.id;
+  const oldUser = users.find((user) => user.id === userID);
+  const updates = req.body;
+
+  if (oldUser) {
+    if (!updates.name || !updates.bio) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+      const indexOldUser = users.findIndex((user) => user.id === userID);
+      users[indexOldUser] = {
+        ...users[indexOldUser],
+        name: updates.name,
+        bio: updates.bio,
+      };
+      res.status(200).json(users[indexOldUser]);
+    }
   }
 });
 
